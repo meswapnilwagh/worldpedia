4995820f7ff07ae507eec8f3520ac4b8471e2959
Swapnil Wagh <wagh.aswapnil@gmail.com>
2022-02-06 22:36:27 +0400

    state module export added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Continents from './modules/continents';
 import * as Subregions from './modules/subregions';
 import * as Countries from './modules/countries';
+import * as States from './modules/states';
 
 export { Continents,Subregions,Countries };
